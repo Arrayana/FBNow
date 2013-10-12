5ce46bffac6cfcb7a5bfac9bf823a60e584b1001
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2013-10-12 01:29:05 -0700

    mod fbnow-lib

--- public/fbnow-lib.js
+++ public/fbnow-lib.js
@@ -1,9 +1,8 @@
 //
 //
 //
-function showResponse(response){
-  console.log(response);
-};
+
+var _radInKM = 1.0;
 
 // Calculate the distance given two points
 function rad(x) {return x*Math.PI/180;}
@@ -23,16 +22,15 @@ function distHaversine(p1, p2) {
 // @MyEvent is an events I'm going to attend.
 // @myLocation is {lat: xxx, long: yyy}
 function isCloseToTheEvent(myLocation, myEvent){
-  var radInKM = 1.0;
+  
   var testMyEvent = {"latitude": 37.482898, "longitude": -122.149909};
   var distance = distHaversine(myLocation, testMyEvent);
-  if(distance <= radInKM){
-    // You're in 1km radious.
-    return true;
-  }else{
-    return false;
-  }
+  return isClosedToLocation(myLocation, testMyEvent);
 };
+
+function isClosedToLocation(location1, location2){
+  return distHaversine(location1, location2) > _radInKM;
+}
 
 // @location: {lat: xxx, long: yyy}
 function getClosestPointOfInterest(location){
@@ -40,7 +38,104 @@ function getClosestPointOfInterest(location){
 };
 
 
-function searchStatusContainingPlaceName(name){
+function searchStatusContainingPlaceName(placeName, statuses, currentLocation){
+  statuses =  {
+    "data": [
+      {
+        "place": {
+          "id": "152339121642221", 
+          "name": "Facebook NorCal Regional Hackathon", 
+          "location": "Facebook HQ", 
+          "start_time": "2013-10-11T17:00:00-0700"
+        }, 
+        "message": "s/sleep/hack/g \nwhat am I doing here?", 
+        "id": "10151987481300955", 
+        "updated_time": "2013-10-12T07:40:01+0000"
+      }, 
+      {
+        "place": {
+          "id": "151674991513447", 
+          "name": "The Flatirons", 
+          "location": {
+            "street": "", 
+            "city": "Boulder", 
+            "state": "CO", 
+            "country": "United States", 
+            "zip": "80302", 
+            "latitude": 39.989807108461, 
+            "longitude": -105.29370546867
+          }
+        }, 
+        "message": "Hello Boulder!!", 
+        "id": "10151697226090955", 
+        "updated_time": "2013-05-28T18:33:17+0000"
+      }, 
+      {
+        "place": {
+          "id": "141231406010147", 
+          "name": "South Dakota State Line", 
+          "location": {
+            "street": "", 
+            "zip": "", 
+            "latitude": 43.304181087223, 
+            "longitude": -104.05363399699
+          }
+        }, 
+        "message": "Hello nowhere!", 
+        "id": "10151695442160955", 
+        "updated_time": "2013-05-27T23:27:09+0000"
+      }, 
+      {
+        "message": "Hello Madison!!", 
+        "id": "10151691644070955", 
+        "updated_time": "2013-05-25T23:00:45+0000"
+      }, 
+      {
+        "message": "Hello Chicago!!", 
+        "id": "10151690131905955", 
+        "updated_time": "2013-05-25T00:44:57+0000"
+      }
+    ],
+  } 
 
-  //return status
+  var data = statuses.data;
+  var foundPlace = false;
+  var radInKM = 1.0;
+
+  for (var i = data.length - 1; i >= 0; i--) {
+    var place = data[i].place;
+
+    // If the status has location already.
+    // Location can be:
+    //  name => string
+    //  noname => object
+    if (place.location != null) {
+      if (typeof place.location == "string"){
+        if (place.location.toLowerCase().indexOf(placeName) >= 0) {
+          foundPlace = true;
+        }else{
+          foundPlace = false;
+        };
+      }else if(typeof place.location == "object"){
+        // Look for closed distance
+        if(currentLocation != null){
+          foundPlace = isClosedToLocation(currentLocation, { latitude: place.location.latitude,
+                                                              longitude: place.location.longitude
+                                                            });
+        }else{
+
+        }
+      }else{
+        console.log("I dont know where you were!!");
+      }
+      
+    }else{ 
+      // No location info
+
+    }
+    
+
+  };
+
+  return foundPlace;
 }
