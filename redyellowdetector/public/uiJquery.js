fb83ecd29af8905b8aee798cd02415657b3fec5e
manuel nakamurakare <manhu@manuel.(none)>
2013-10-12 00:03:29 -0700

    moved file

--- redyellowdetector/public/uiJquery.js
+++
@@ -1,9 +0,0 @@
- $( "#message1" ).click(function() {alert("ss")})
- 
- $( "#message1" ).on( "swipeleft", swipeleftHandle
-  // Callback function references the event target and adds the 'swipeleft' class to it
-  function swipeleftHandler( event ){
-    alert("message1")
-
-    //$( event.target ).addClass( "swipeleft" );
-  }
