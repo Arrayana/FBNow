7caf1141660fe38b4c68f6ca77f3a04f8e5c1a5f
Knott <wittawat@cmu.edu>
2013-10-12 09:52:51 -0700

    Fix fbnow-lib.js

--- public/fbnow-lib.js
+++ public/fbnow-lib.js
@@ -30,115 +30,156 @@ function isCloseToTheEvent(myLocation, myEvent){
 
 function isClosedToLocation(location1, location2){
   return distHaversine(location1, location2) > _radInKM;
-}
-
-// @location: {lat: xxx, long: yyy}
-function getClosestPointOfInterest(location){
-
 };
 
 
-function searchStatusContainingPlaceName(placeName, statuses, currentLocation){
-  statuses =  {
-    "data": [
-      {
-        "place": {
-          "id": "152339121642221", 
-          "name": "Facebook NorCal Regional Hackathon", 
-          "location": "Facebook HQ", 
-          "start_time": "2013-10-11T17:00:00-0700"
-        }, 
-        "message": "s/sleep/hack/g \nwhat am I doing here?", 
-        "id": "10151987481300955", 
-        "updated_time": "2013-10-12T07:40:01+0000"
-      }, 
-      {
-        "place": {
-          "id": "151674991513447", 
-          "name": "The Flatirons", 
-          "location": {
-            "street": "", 
-            "city": "Boulder", 
-            "state": "CO", 
-            "country": "United States", 
-            "zip": "80302", 
-            "latitude": 39.989807108461, 
-            "longitude": -105.29370546867
-          }
-        }, 
-        "message": "Hello Boulder!!", 
-        "id": "10151697226090955", 
-        "updated_time": "2013-05-28T18:33:17+0000"
-      }, 
-      {
-        "place": {
-          "id": "141231406010147", 
-          "name": "South Dakota State Line", 
-          "location": {
-            "street": "", 
-            "zip": "", 
-            "latitude": 43.304181087223, 
-            "longitude": -104.05363399699
-          }
-        }, 
-        "message": "Hello nowhere!", 
-        "id": "10151695442160955", 
-        "updated_time": "2013-05-27T23:27:09+0000"
-      }, 
-      {
-        "message": "Hello Madison!!", 
-        "id": "10151691644070955", 
-        "updated_time": "2013-05-25T23:00:45+0000"
-      }, 
-      {
-        "message": "Hello Chicago!!", 
-        "id": "10151690131905955", 
-        "updated_time": "2013-05-25T00:44:57+0000"
-      }
-    ],
-  } 
-
-  var data = statuses.data;
-  var foundPlace = false;
-  var radInKM = 1.0;
-
-  for (var i = data.length - 1; i >= 0; i--) {
-    var place = data[i].place;
-
-    // If the status has location already.
-    // Location can be:
-    //  name => string
-    //  noname => object
-    if (place.location != null) {
-      if (typeof place.location == "string"){
-        if (place.location.toLowerCase().indexOf(placeName) >= 0) {
-          foundPlace = true;
-        }else{
-          foundPlace = false;
-        };
-      }else if(typeof place.location == "object"){
-        // Look for closed distance
-        if(currentLocation != null){
-          foundPlace = isClosedToLocation(currentLocation, { latitude: place.location.latitude,
-                                                              longitude: place.location.longitude
-                                                            });
-        }else{
-
-        }
-      }else{
-        console.log("I dont know where you were!!");
-      }
-      
-    }else{ 
-      // No location info
-
+// @return
+//  Object {category: "Local business", category_list: Array[1], location: Object, name: "Fuki Sushi at Facebook", id: "175517295918833"}
+function getClosestPointOfInterest(currentLocation, callback){
+  var searchString = 'search?type=place&distance=1000&center=' + 
+                      currentLocation.latitude.toFixed(8) + 
+                      "," +
+                      currentLocation.longitude.toFixed(8);
+  FB.api(searchString, function(response) {
+    // get the first POI
+    if (response.data[0]){
+       if (typeof callback=="function") callback(response.data[0]);
+    }else{
+       if (typeof callback=="function") callback(null);
     }
+  });
+};
+
+// @Place is {category: "Local business", category_list: Array[1], location: Object, name: "Fuki Sushi at Facebook", id: "175517295918833"}
+// @return Object {place: Object, message: "s/sleep/hack/g ↵what am I doing here?", id: "10151987481300955", updated_time: "2013-10-12T07:40:01+0000"}
+function searchStatusContainingPlace(place, callback){
+  var searchString = 'me?fields=id,name,statuses.fields(place,message)';
+  FB.api(searchString, function(response) {
+    var statuses = response.statuses.data;
+    console.log(statuses);
+    // Loop to compare place id
+    var foundStatus = null;
+    for (var i = statuses.length - 1; i >= 0; i--) {
+      if (statuses[i].id == place.id){
+        foundStatus = statuses[i].id;
+      }
+    };
+
+    callback(foundStatus);
+  });
+};
+
+// @pointOfInterest
+// Object {category: "Local business", category_list: Array[1], location: Object, name: "Fuki Sushi at Facebook", id: "175517295918833"}
+function searchStatusContainingPointOfInterest(pointOfInterest, callback){
+  searchStatusContainingPlace(pointOfInterest, callback);
+}
+
+// placename is a string
+// function searchStatusContainingPlaceName(placeName, statuses, currentLocation){
+
+
+
+//   statuses =  {
+//     "data": [
+//       {
+//         "place": {
+//           "id": "152339121642221", 
+//           "name": "Facebook NorCal Regional Hackathon", 
+//           "location": "Facebook HQ", 
+//           "start_time": "2013-10-11T17:00:00-0700"
+//         }, 
+//         "message": "s/sleep/hack/g \nwhat am I doing here?", 
+//         "id": "10151987481300955", 
+//         "updated_time": "2013-10-12T07:40:01+0000"
+//       }, 
+//       {
+//         "place": {
+//           "id": "151674991513447", 
+//           "name": "The Flatirons", 
+//           "location": {
+//             "street": "", 
+//             "city": "Boulder", 
+//             "state": "CO", 
+//             "country": "United States", 
+//             "zip": "80302", 
+//             "latitude": 39.989807108461, 
+//             "longitude": -105.29370546867
+//           }
+//         }, 
+//         "message": "Hello Boulder!!", 
+//         "id": "10151697226090955", 
+//         "updated_time": "2013-05-28T18:33:17+0000"
+//       }, 
+//       {
+//         "place": {
+//           "id": "141231406010147", 
+//           "name": "South Dakota State Line", 
+//           "location": {
+//             "street": "", 
+//             "zip": "", 
+//             "latitude": 43.304181087223, 
+//             "longitude": -104.05363399699
+//           }
+//         }, 
+//         "message": "Hello nowhere!", 
+//         "id": "10151695442160955", 
+//         "updated_time": "2013-05-27T23:27:09+0000"
+//       }, 
+//       {
+//         "message": "Hello Madison!!", 
+//         "id": "10151691644070955", 
+//         "updated_time": "2013-05-25T23:00:45+0000"
+//       }, 
+//       {
+//         "message": "Hello Chicago!!", 
+//         "id": "10151690131905955", 
+//         "updated_time": "2013-05-25T00:44:57+0000"
+//       }
+//     ],
+//   } 
+
+//   var data = statuses.data;
+//   var foundPlace = false;
+
+//   for (var i = data.length - 1; i >= 0; i--) {
+//     var place = data[i].place;
+
+//     // If the status has location already.
+//     // Location can be:
+//     //  name => string
+//     //  noname => object
+//     if (place.location != null) {
+//       if (typeof place.location == "string"){
+//         if (place.location.toLowerCase().indexOf(placeName) >= 0) {
+//           foundPlace = true;
+//         }else{
+//           foundPlace = false;
+//         };
+//       }else if(typeof place.location == "object"){
+//         // Look for closed distance
+//         if(currentLocation != null){
+//           foundPlace = isClosedToLocation(currentLocation, { latitude: place.location.latitude,
+//                                                               longitude: place.location.longitude
+//                                                             });
+//         }else{
+
+//         }
+//       }else{
+//         console.log("I dont know where you were!!");
+//       }
+      
+//     }else{ 
+//       // No location info
+
+//     }
     
 
-  };
+//   };
 
-  return foundPlace;
-}
+//   return foundPlace;
+// }
+
 
 // http://stackoverflow.com/questions/2782976/convert-facebook-date-format-to-javascript-date
 function timeAgo(time){
@@ -227,3 +268,4 @@ function getTemperatureDiff(currentLocation, callback){
 
 
 }
+
